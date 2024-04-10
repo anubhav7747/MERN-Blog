7d00da24aaafba588f312517db3b78b8f02137bb
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-04-10 22:33:17 +0530

    Add create a post API route.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -22,11 +23,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000!");
+  console.log("Server is listening on port 3000!");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode || 500;
