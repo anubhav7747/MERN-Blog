2c3e588b97230a3b0d43d72cba601e877695e85c
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-04-24 00:01:38 +0530

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,12 +4,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
   ],
 };
 
